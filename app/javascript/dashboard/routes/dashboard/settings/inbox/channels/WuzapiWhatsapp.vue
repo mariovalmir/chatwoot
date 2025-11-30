df4b502b62505794e946f377cb36139dd8620aa9
mario-pereira <mario-pereira@procergs.rs.gov.br>
2025-11-30 16:29:13 -0300

    feat: adiciona seção ADVANCED_OPTIONS ao formulário Wuzapi

--- app/javascript/dashboard/routes/dashboard/settings/inbox/channels/WuzapiWhatsapp.vue
+++ app/javascript/dashboard/routes/dashboard/settings/inbox/channels/WuzapiWhatsapp.vue
@@ -6,7 +6,7 @@ import { useI18n } from 'vue-i18n';
 import { useVuelidate } from '@vuelidate/core';
 import { useAlert } from 'dashboard/composables';
 import { required } from '@vuelidate/validators';
-import { isPhoneE164OrEmpty } from 'shared/helpers/Validators';
+import { isPhoneE164OrEmpty, isValidURL } from 'shared/helpers/Validators';
 
 import NextButton from 'dashboard/components-next/button/Button.vue';
 
@@ -16,18 +16,29 @@ const { t } = useI18n();
 
 const inboxName = ref('');
 const phoneNumber = ref('');
+const apiUrl = ref('');
+const token = ref('');
+const showAdvancedOptions = ref(false);
 
 const uiFlags = computed(() => store.getters['inboxes/getUIFlags']);
 
 const rules = computed(() => ({
   inboxName: { required },
   phoneNumber: { required, isPhoneE164OrEmpty },
+  apiUrl: { isValidURL },
+  token: {},
 }));
 
 const v$ = useVuelidate(rules, {
   inboxName,
   phoneNumber,
+  apiUrl,
+  token,
 });
+
+const setShowAdvancedOptions = () => {
+  showAdvancedOptions.value = true;
+};
 
 const createChannel = async () => {
   v$.value.$touch();
@@ -36,13 +47,21 @@ const createChannel = async () => {
   }
 
   try {
+    const providerConfig = {};
+    if (apiUrl.value) {
+      providerConfig.api_url = apiUrl.value;
+    }
+    if (token.value) {
+      providerConfig.user_token = token.value;
+    }
+
     const whatsappChannel = await store.dispatch('inboxes/createChannel', {
       name: inboxName.value,
       channel: {
         type: 'whatsapp',
         phone_number: phoneNumber.value,
         provider: 'wuzapi',
-        provider_config: {},
+        provider_config: providerConfig,
       },
     });
 
@@ -90,6 +109,55 @@ const createChannel = async () => {
         </span>
       </label>
     </div>
+
+    <div
+      v-if="!showAdvancedOptions"
+      class="w-[65%] flex-shrink-0 flex-grow-0 max-w-[65%] mb-4"
+    >
+      <NextButton icon="i-lucide-plus" sm link @click="setShowAdvancedOptions">
+        {{ $t('INBOX_MGMT.ADD.WHATSAPP.ADVANCED_OPTIONS') }}
+      </NextButton>
+    </div>
+    <template v-else>
+      <div class="w-[65%] flex-shrink-0 flex-grow-0 max-w-[65%]">
+        <span class="text-sm text-gray-600">
+          {{ $t('INBOX_MGMT.ADD.WHATSAPP.ADVANCED_OPTIONS') }}
+        </span>
+        <label :class="{ error: v$.apiUrl.$error }">
+          {{ $t('INBOX_MGMT.ADD.WHATSAPP.API_URL.LABEL') }}
+          <input
+            v-model="apiUrl"
+            type="text"
+            :placeholder="$t('INBOX_MGMT.ADD.WHATSAPP.API_URL.PLACEHOLDER')"
+            @blur="v$.apiUrl.$touch"
+          />
+          <span v-if="v$.apiUrl.$error" class="message">
+            {{ $t('INBOX_MGMT.ADD.WHATSAPP.API_URL.ERROR') }}
+          </span>
+          <p class="help-text">
+            {{ $t('INBOX_MGMT.ADD.WHATSAPP.API_URL.HELP') }}
+          </p>
+        </label>
+      </div>
+
+      <div class="w-[65%] flex-shrink-0 flex-grow-0 max-w-[65%]">
+        <label :class="{ error: v$.token.$error }">
+          {{ $t('INBOX_MGMT.ADD.WHATSAPP.TOKEN.LABEL') }}
+          <input
+            v-model="token"
+            type="password"
+            :placeholder="$t('INBOX_MGMT.ADD.WHATSAPP.TOKEN.PLACEHOLDER')"
+            @blur="v$.token.$touch"
+          />
+          <span v-if="v$.token.$error" class="message">
+            {{ $t('INBOX_MGMT.ADD.WHATSAPP.TOKEN.ERROR') }}
+          </span>
+          <p class="help-text">
+            {{ $t('INBOX_MGMT.ADD.WHATSAPP.TOKEN.HELP') }}
+          </p>
+        </label>
+      </div>
+    </template>
 
     <div class="w-full">
       <NextButton
